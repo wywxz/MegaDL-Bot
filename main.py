9a097adfae237f2c5cf72730344826f52c0a76c7
wywxz <53309090+wywxz@users.noreply.github.com>
2022-12-10 20:36:40 -0400

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import os
 import asyncio
 from config import Config
 from pyrogram import Client, idle
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(_):
+    return web.json_response(
+        {
+            "server_status": "running"
+        }
+    )
+
+def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
 
 if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
@@ -19,6 +35,9 @@ if __name__ == "__main__" :
     )
     app.start()
     print('\n\n>>> MegaDL-Bot Started. Join @AsmSafone!')
+    server = web.AppRunner(web_server())
+    await server.setup()
+    await web.TCPSite(server, "0.0.0.0", 8080).start()
     idle()
     app.stop()
     print('\n\n>>> MegaDL-Bot Stopped. Join @AsmSafone!')
